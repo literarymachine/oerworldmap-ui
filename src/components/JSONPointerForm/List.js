9fe6d418bd66cb87934c0e8046db8d33ebd7f9bf
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-02-18 22:37:23 +0100

    Checkbox hack based new item control

--- src/components/JSONPointerForm/List.js
+++ src/components/JSONPointerForm/List.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import ListItem from './ListItem'
 import withFormData from './withFormData'
 
-const List = ({value, children}) => (
+const List = ({name, value, children}) => (
   <ul>
     {value.map((item, index) => (
       <ListItem property={index} key={index}>
@@ -12,12 +12,23 @@ const List = ({value, children}) => (
       </ListItem>
     ))}
     <ListItem property={value.length} key={value.length}>
-      {React.cloneElement(children)}
+      <div className="newItemWrapper">
+        <input
+          type="checkbox"
+          key={`${name}-${value.length}`}
+          className="formControl" id={`${name}-toggle`}
+        />
+        <label htmlFor={`${name}-toggle`}>Add {name}</label>
+        <div className="newItem">
+          {React.cloneElement(children)}
+        </div>
+      </div>
     </ListItem>
   </ul>
 )
 
 List.propTypes = {
+  name: PropTypes.string.isRequired,
   value: PropTypes.arrayOf(PropTypes.any),
   children: PropTypes.element.isRequired
 }
