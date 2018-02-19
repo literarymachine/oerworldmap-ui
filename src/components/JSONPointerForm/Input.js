283ac0d58e1f53b0002b19ae8b9eb384b29f3df7
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-02-19 22:56:36 +0100

    Only insert element, don't update value on 'add'

--- src/components/JSONPointerForm/Input.js
+++ src/components/JSONPointerForm/Input.js
@@ -3,17 +3,35 @@ import PropTypes from 'prop-types'
 
 import withFormData from './withFormData'
 
+let changed = null
+const autoFocus = (name) => {
+  const focus = changed === name
+  changed = focus ? null : changed
+  return focus
+}
+
+const onFocus = (e) => {
+  const tmp = e.target.value
+  e.target.value = ''
+  e.target.value = tmp
+}
+
 const Input = ({type, name, value, setValue}) => (
   <input
     type={type}
     name={name}
     value={value}
     placeholder={name}
-    onChange={e => setValue(
-      e.target.type === 'checkbox'
-        ? (e.target.checked ? "true" : null)
-        : e.target.value
-    )}
+    autoFocus={autoFocus(name)}
+    onFocus={onFocus}
+    onChange={(e) => {
+      changed = name
+      setValue(
+        e.target.type === 'checkbox'
+          ? (e.target.checked ? "true" : null)
+          : e.target.value
+      )
+    }}
   />
 )
 
--- src/components/JSONPointerForm/List.js
+++ src/components/JSONPointerForm/List.js
@@ -4,20 +4,47 @@ import PropTypes from 'prop-types'
 import ListItem from './ListItem'
 import withFormData from './withFormData'
 
-const List = ({name, value, children, setValue, defaultValue}) => (
-  <ul>
-    {value.map((item, index) => (
-      <ListItem property={index} key={index}>
-        {React.cloneElement(children)}
-      </ListItem>
-    ))}
-    <li>
-      <button type="button" onClick={() => setValue(value.concat({}))}>
-        Add {name}
-      </button>
-    </li>
-  </ul>
-)
+class List extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.insert = null
+  }
+
+  componentDidUpdate() {
+    this.insert = null
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.value.map((item, index) => (
+          <ListItem property={index} key={index}>
+            {React.cloneElement(this.props.children)}
+          </ListItem>
+        ))}
+        {this.insert ? this.insert : (
+          <li>
+            <button
+              type="button"
+              onClick={() => {
+                this.insert = (
+                  <ListItem property={this.props.value.length} key={this.props.value.length}>
+                    {React.cloneElement(this.props.children)}
+                  </ListItem>
+                )
+                this.forceUpdate()
+              }}
+            >
+              Add {this.props.name}
+            </button>
+          </li>
+        )}
+      </ul>
+    )
+  }
+
+}
 
 List.propTypes = {
   name: PropTypes.string.isRequired,
