a209ee5e6c39e9a8860eb7b754b5b852f205b8d8
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-02-20 15:39:47 +0100

    Interim commit

--- src/components/JSONPointerForm/Builder.js
+++ src/components/JSONPointerForm/Builder.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import jsonPointer from 'json-pointer'
 import merge from 'deepmerge'
+import Ajv from 'ajv'
 
 import Form from './Form'
 import Fieldset from './Fieldset'
@@ -10,71 +11,97 @@ import List from './List'
 
 const cloneSchema = (schema) => JSON.parse(JSON.stringify(schema))
 
-const getSchema = (root, ptr) => expandSchema(
-  root, cloneSchema(jsonPointer.get(root, ptr.slice(1)))
-)
+class Builder extends React.Component {
 
-const expandSchema = (root, schema) => {
-  schema = resolveRefs(root, schema)
-  if ('items' in schema) {
-    schema.items = expandSchema(root, schema.items)
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: {}
+    }
+    this.ajv = new Ajv({
+      schemaId: 'id',
+      allErrors: true,
+      jsonPointers: true
+    })
+    this.ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
+    this.instanceSchema = this.getSchema('#/definitions/Organization')
+    console.info(this.instanceSchema)
+    this.validate = this.ajv.compile(this.instanceSchema)
+    this.formComponents = this.processSchema(this.instanceSchema)
   }
-  if ('allOf' in schema) {
-    schema.allOf.forEach((allOf) =>
-      schema = merge(schema, expandSchema(root, allOf))
-    )
-    delete schema.allOf
-  }
-  Object.keys(schema.properties || {}).forEach((property) => {
-    schema.properties[property] = expandSchema(
-      root, schema.properties[property]
-    )
-  })
-  return schema
-}
 
-const resolveRefs = (root, schema) => {
-  if ('$ref' in schema) {
-    schema = merge(getSchema(root, schema['$ref']), schema)
-    delete schema['$ref']
-  }
-  return schema
-}
+  onSubmit = (data) => this.validate(data)
+    ? console.log("valid", data)
+    : this.setState({errors: this.validate.errors}) &&
+      console.warn("invalid", this.validate.errors, data)
 
-const processSchema = (schema) => {
-  switch (schema.type) {
-    case 'string':
-      return <Input type="text" />
-    case 'integer':
-    case 'number':
-      return <Input type="number" />
-    case 'boolean':
-      return <Input type="checkbox" />
-    case 'array':
-      return <List>{processSchema(schema.items)}</List>
-    case 'object':
-      return (
-        <Fieldset>
-          {Object.keys(schema.properties).map((property) => React.cloneElement(
-            processSchema(schema.properties[property]), {
-              property, key: property
-            }
-          ))}
-        </Fieldset>
+  getSchema = (ptr) => this.expandSchema(
+    cloneSchema(jsonPointer.get(this.props.schema, ptr.slice(1)))
+  )
+
+  expandSchema = (schema) => {
+    schema = this.resolveRefs(schema)
+    if ('items' in schema) {
+      schema.items = this.expandSchema(schema.items)
+    }
+    if ('allOf' in schema) {
+      schema.allOf.forEach((allOf) =>
+        schema = merge(schema, this.expandSchema(allOf))
       )
-    case 'null':
-    default:
-      console.warn('Could not determine form component for', schema)
-      return <Input type="text" />
+      delete schema.allOf
+    }
+    Object.keys(schema.properties || {}).forEach((property) => {
+      schema.properties[property] = this.expandSchema(
+        schema.properties[property]
+      )
+    })
+    return schema
   }
-}
 
-const Builder = ({schema}) => (
-  <Form>
-    {console.log(getSchema(schema, '#/definitions/Organization'))}
-    {processSchema(getSchema(schema, '#/definitions/Organization'))}
-  </Form>
-)
+  resolveRefs = (schema) => {
+    if ('$ref' in schema) {
+      schema = merge(this.getSchema(schema['$ref']), schema)
+      delete schema['$ref']
+    }
+    return schema
+  }
+
+  processSchema = (schema) => {
+    switch (schema.type) {
+      case 'string':
+        return <Input type="text" />
+      case 'integer':
+      case 'number':
+        return <Input type="number" />
+      case 'boolean':
+        return <Input type="checkbox" />
+      case 'array':
+        return <List>{this.processSchema(schema.items)}</List>
+      case 'object':
+        return (
+          <Fieldset>
+            {Object.keys(schema.properties).map((property) => React.cloneElement(
+              this.processSchema(schema.properties[property]), {
+                property, key: property
+              }
+            ))}
+          </Fieldset>
+        )
+      case 'null':
+      default:
+        console.warn('Could not determine form component for', schema)
+        return <Input type="text" />
+    }
+  }
+
+  render = () => (
+    <Form onSubmit={this.onSubmit} errors={this.state.errors}>
+      {this.formComponents}
+    </Form>
+  )
+
+
+}
 
 Builder.propTypes = {
   schema: PropTypes.objectOf(PropTypes.any)
